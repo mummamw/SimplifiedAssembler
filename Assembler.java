0549c4e2e7cbe28a7eb8ad258178c329d7ca457f
Michael Mumma <mummamw@miamioh.edu>
2015-10-17 22:26:24 -0400

    Setting up

---
+++ Assembler.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.io.*;
+
+public class Assembler {
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+		// Opening input file 
+		File inputFile = new File(args[0]);
+		//Scanner fileScanner = new Scanner(new File(args[0]));
+		BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+
+		// Creating output file
+		File outputFile = new File(args[1]);
+		PrintWriter writer = new PrintWriter(outputFile);
+		
+		//Generating Hash tables for maps
+		//registerHash used for getting register's values
+		Hashtable<String, Integer> registerHash = new Hashtable<String, Integer>();
+			registerHash.put("$zero", 00000);
+			registerHash.put("$at", 00001);
+			registerHash.put("$v0", 00010);
+			registerHash.put("$v1", 00011);
+			registerHash.put("$a0", 00100);
+			registerHash.put("$a1", 00101);
+			registerHash.put("$a2", 00110);
+			registerHash.put("$a3", 00111);
+			registerHash.put("$t0", 01000);
+			registerHash.put("$t1", 01001);
+			registerHash.put("$t2", 01010);
+			registerHash.put("$t3", 01011);
+			registerHash.put("$t4", 01100);
+			registerHash.put("$t5", 01101);
+			registerHash.put("$t6", 01110);
+			registerHash.put("$t7", 01111);
+			registerHash.put("$s0", 10000);
+			registerHash.put("$s1", 10001);
+			registerHash.put("$s2", 10010);
+			registerHash.put("$s3", 10011);
+			registerHash.put("$s4", 10100);
+			registerHash.put("$s5", 10101);
+			registerHash.put("$s6", 10110);
+			registerHash.put("$s7", 10111);
+			registerHash.put("$t8", 11000);
+			registerHash.put("$t9", 11001);
+	
+		// Hash Table for J format instructions
+		//op codes should always be 6 bits
+		Hashtable<String, Integer> jHash = new Hashtable<String, Integer>();
+			jHash.put("j",   000010);
+			jHash.put("jal", 000011);
+
+		// Hash Table for I format instructions 
+		//op codes should always be 6 bits
+		Hashtable<String, Integer> iHash = new Hashtable<String, Integer>();
+			iHash.put("lw",   100011);
+			iHash.put("sw",   101011);
+			iHash.put("andi", 001100);
+			iHash.put("ori",  001101);
+			iHash.put("lui",  001111);
+			iHash.put("beq",  000100);
+			iHash.put("stli", 001010);
+			iHash.put("addi", 001000);
+		
+		// Hash Table for R format instructions
+		// Mapping "function" field here
+		Hashtable<String, Integer> rHash = new Hashtable<String, Integer>();
+			rHash.put("add", 100000);
+			rHash.put("sub", 100001);
+			rHash.put("and", 100100);
+			rHash.put("or",  100101);
+			rHash.put("sll", 000000);
+			rHash.put("slt", 101010);
+			rHash.put("srl", 000010);
+			rHash.put("jr",  001000);
+	
+		//While loop containing more lines
+		
+			//Flawed in the case of an empty file being passed in
+			String line = reader.readLine();
+			while( (line) != null) {
+			    System.out.println(line);
+			    
+			    try{
+			    	line = reader.readLine();
+			    } catch (IOException e) {
+			    	System.out.println(e.toString());
+			    }
+			}
+		
+		
+			
+	}
+	
+}
